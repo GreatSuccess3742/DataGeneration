433602004ddfa19a2837bb93070e60a83ef088c2
GreatSuccess3742 <hsie626@usc.edu>
2017-04-05 17:49:52 -0700

    Update Binarizing Script
    1.Now handle different file type
    2.Hole filling and bmareaopen(remove single dot) on .jpg file

--- GT_Binarize.m
+++ GT_Binarize.m
@@ -1,19 +1,41 @@
 clc;
 clear;
 mystr = dir('*/*GT*');
+
 %for statement
 [M,N] = size(mystr);
 for file_index = 1:M
     filename = mystr(file_index).name;
-    input = imread(filename);
-    input = input(:,:,1);
-    %Binarized = input > 0;
-    Binarized = imbinarize(input,0.5);
-    %remove the extension of the filename
-    [pathstr,name,ext] = fileparts(filename);
-    %output directory
-    path = '../Binarized/';
-    name = strcat(path,name,'_Binarized.png');
-    %name = strcat(path,name,'_Binarized');
-    imwrite(Binarized,name);
+    if size(findstr(filename,'png')) > 0
+        [data_,map_,input] = imread(filename);
+        if(size(input) > 0)
+            Binarized = imcomplement(imbinarize(input,0.5));
+        else
+            input = data_(:,:,1);
+            Binarized = (imbinarize(input,0.5));
+        end
+        %remove the extension of the filename
+        [pathstr,name,ext] = fileparts(filename);
+        %output directory
+        path = '../Binarized/';
+        name = strcat(path,name,'_Binarized.png');
+        imwrite(Binarized,name);
+    elseif size(findstr(filename,'jpg')) > 0
+        fprintf('%s file extension is jpg\n',filename);
+        input = imread(filename);
+        input  = input(:,:,1);
+        Binarized = (imbinarize(input,0.5));
+        
+        [pathstr,name,ext] = fileparts(filename);
+        path = '../Binarized/';
+        name = strcat(path,name,'_Binarized.png');
+        Binarized = imcomplement(Binarized);
+        Binarized = bwareaopen(Binarized,20,4);
+        Binarized = imfill(Binarized,'holes');
+        Binarized = imcomplement(Binarized);
+        imwrite(Binarized,name);
+    elseif size(findstr(filename,'xcf')) > 0
+        fprintf('file extension is xcf\n');   
+    end
+
 end
